f0370ba86605787649ccb996d7aa6f14e74cd2ef
aman.shrivastava <aman.shrivastava@flipkart.com>
2017-08-11 03:55:17 +0530

    dress shoulders

--- src/main/scala/com/flipkart/fashion/services/IPService.scala
+++ src/main/scala/com/flipkart/fashion/services/IPService.scala
@@ -31,6 +31,71 @@ object IPService {
   val Cr_MIN = 135
   val Cr_MAX = 180
 
+  def getContourImage(file: String) = {
+    val image = Highgui.imread(file)
+
+    val image_bw: Mat = image.clone()
+
+    Highgui.imwrite(s"/Users/$username/Pictures/image_bw.jpg", image_bw)
+    Imgproc.blur(image_bw, image_bw, new Size(3,3))
+    val canny_out: Mat = image.clone()
+
+    Imgproc.Canny(image_bw,canny_out, 100, 200, 3, false)
+    val contours = new util.ArrayList[MatOfPoint]()
+
+    val hierarchy = new Mat()
+    Imgproc.findContours(canny_out,contours,hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE, new Point(0, 0))
+
+    /// Draw contours
+    val drawing = Mat.zeros( canny_out.size(), CvType.CV_8UC3 )
+    println(contours(0).toList.head, contours(0).toList.last)
+    for(  i <- 0 until  contours.size()  )
+    {
+      val color = new Scalar( 125, 200, 225 )
+      Imgproc.drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, new Point(0,0) )
+    }
+
+    drawing
+  }
+
+  def leftShoulder(dressImage: Mat): Point = {
+
+    for (i <- 0 until dressImage.height()) {
+      for (j <- 0 until dressImage.width()) {
+
+        if (!dressImage.get(i, j).contains(0.0) && j < dressImage.width() / 2 ) {
+          return new Point(j, i)
+        }
+
+      }
+    }
+
+    null
+  }
+
+
+  def rightShoulder(dressImage: Mat): Point = {
+
+    for (i <- 0 until dressImage.height()) {
+      for (j <- dressImage.width() - 1 to 0 by -1) {
+        if (!dressImage.get(i, j).contains(0.0) && j > dressImage.width() / 2) {
+          return new Point(j, i)
+        }
+      }
+    }
+
+    null
+  }
+
+
+  def shoulders(image: Mat): (Point, Point) = {
+    (leftShoulder(image), rightShoulder(image))
+  }
+
+  def shoulders(file: String): (Point, Point) = {
+    shoulders(getContourImage(file))
+  }
+
 
   def neckMidPoint(file: String): Point = {
 
@@ -41,12 +106,10 @@ object IPService {
     val hsDetections = new MatOfRect
     val faceDetections = new MatOfRect
     hsDetector.detectMultiScale(image, hsDetections)
-    //    bodyDetector.detectMultiScale(image, bodyDetections, 1.01, 2, Objdetect.CASCADE_SCALE_IMAGE , new Size(200,humanHeight), new Size())
 
     val image_bw: Mat = image.clone()
 
     Imgproc.cvtColor(image, image_bw, Imgproc.COLOR_BGR2GRAY)
-    //    Core.inRange(image_bw, new Scalar(Y_MIN, Cr_MIN, Cb_MIN), new Scalar(Y_MAX, Cr_MAX, Cb_MAX), image_skin)
 
     faceDetector.detectMultiScale(image_bw, faceDetections, 1.01, 2, Objdetect.CASCADE_FIND_BIGGEST_OBJECT , new Size(), new Size())
 
@@ -81,24 +144,18 @@ object IPService {
   def detectBody(file:String): Unit ={
 
     val image = Highgui.imread(file)
-    val bodyDetector = new CascadeClassifier(CASCADE_FILE_FULL_BODY)
     val hsDetector = new CascadeClassifier(CASCADE_FILE_HS)
     val faceDetector = new CascadeClassifier(CASCADE_FILE_FACE)
 
     val imageHeight = image.height()
-    val humanHeight = imageHeight * 0.7
 
-    val bodyDetections = new MatOfRect
     val hsDetections = new MatOfRect
     val faceDetections = new MatOfRect
     hsDetector.detectMultiScale(image, hsDetections)
 //    bodyDetector.detectMultiScale(image, bodyDetections, 1.01, 2, Objdetect.CASCADE_SCALE_IMAGE , new Size(200,humanHeight), new Size())
     println("Detected "+hsDetections.toArray.length+" body")
 
-
-
     val image_bw: Mat = image.clone()
-    val image_skin: Mat = image.clone()
 
     Imgproc.cvtColor(image, image_bw, Imgproc.COLOR_BGR2GRAY)
 //    Core.inRange(image_bw, new Scalar(Y_MIN, Cr_MIN, Cb_MIN), new Scalar(Y_MAX, Cr_MAX, Cb_MAX), image_skin)
@@ -166,10 +223,8 @@ object IPService {
     Imgproc.cvtColor(personImage,personImage, Imgproc.COLOR_RGB2RGBA)
     val dressImage = Highgui.imread(dressFile, Highgui.CV_LOAD_IMAGE_UNCHANGED)
 
-
-
     val output =  Mat.zeros( personImage.size(), CvType.CV_8UC4 )
-    IPJService.overlayImage(personImage, dressImage, output, new Point(0,0))
+    IPJService.overlayImage(personImage, dressImage, output, neckMidPoint(personFile))
 
     Highgui.imwrite(resultFile, output)
 
